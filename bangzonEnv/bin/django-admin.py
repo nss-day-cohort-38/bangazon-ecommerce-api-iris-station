d8654c9e7627974854cb0f527ac09e11e108bcb7
Kurt Krafft <kkrafft6609@gmail.com>
2020-05-26 10:22:00 -0500

    skeleton for products

---
+++ bangzonEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kurt/workspace/bangazon/2_bangazon/bangazon-ecommerce-api-iris-station/bangzonEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ ecommerceapi/views/products/products.py
@@ -0,0 +1,40 @@
+'''
+A django page to handle all product fetch calls
+
+'''
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from ecommerceapi.models import Product
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for products
+
+    Arguments:
+        serializers
+    """
+    class Meta:
+        model = Product
+        url = serializers.HyperlinkedIdentityField(
+            view_name='products',
+            lookup_field='id'
+        )
+        fields = ('id', 'title', 'price', 'description', 'quantity', "location", 'image_path', 'product_type_id')
+        depth = 1
+
+class Products(ViewSet):
+
+    def create(self, request):
+
+    
+    def list(self, request):
+
+    
+    def retrieve(self, request, pk=None):
+
+    def update(self, request, pk=None): 
+
+
+    def destroy:(self, request, pk=None):
