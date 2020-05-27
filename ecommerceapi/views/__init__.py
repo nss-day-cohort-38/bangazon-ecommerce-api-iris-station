ab495e6a7c54db566b64e901bf85f57acb57d3b5
Andrew Green <green.andrew.b@gmail.com>
2020-05-27 13:27:22 -0500

    debugging

--- ecommerceapi/views/__init__.py
+++ ecommerceapi/views/__init__.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 from .payment import PaymentSerializer, Payments
 from .register import register_user, login_user
->>>>>>> master
 from .product import Products
 from .product_types import ProductTypes
 from .register import register_user, login_user
