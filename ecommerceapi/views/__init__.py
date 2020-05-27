588a4c577a642e2763740ae2a4f8ae79ea1779ea
keithrpotempa <keithrpotempa@gmail.com>
2020-05-27 14:35:28 -0500

    split register and login views into different files, moved them into a subfolder

--- ecommerceapi/views/__init__.py
+++ ecommerceapi/views/__init__.py
@@ -1,5 +1,5 @@
 from .payment import PaymentSerializer, Payments
-from .register import register_user, login_user
+from .customer import register_user, login_user
 from .product import Products
 from .order import Orders
 from .order_product import OrderProducts
