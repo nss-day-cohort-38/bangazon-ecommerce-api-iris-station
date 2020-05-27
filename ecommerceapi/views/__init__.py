60af829509f9f99c7e90468888d7d44031d932d8
Kurt Krafft <kkrafft6609@gmail.com>
2020-05-27 13:38:47 -0500

    added order product relationship view with create and list and order create

--- ecommerceapi/views/__init__.py
+++ ecommerceapi/views/__init__.py
@@ -2,3 +2,4 @@ from .payment import PaymentSerializer, Payments
 from .register import register_user, login_user
 from .product import Products
 from .order import Orders
+from .order_product import OrderProducts
--- ecommerceapi/views/order/orders.py
+++ ecommerceapi/views/order/orders.py
@@ -3,6 +3,7 @@ A django page to handle all order fetch calls
 
 '''
 from django.http import HttpResponseServerError
+import datetime
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
@@ -26,6 +27,11 @@ class Orders(ViewSet):
     
     def list(self, request):
 
+        '''
+        This function returns all of the orders associated with the current user, (based of token). It is important to note that it is ordered
+        by date so the first item returned is the most recent and if that payment type id is null than it is an open cart
+        '''
+
         customer = Customer.objects.get(user=request.auth.user)
         orders = Order.objects.all()
         orders = orders.filter(customer = customer)
@@ -35,6 +41,25 @@ class Orders(ViewSet):
         serializer = OrderSerializer(orders, many=True, context={'request': request})
 
         return Response(serializer.data)
+    
+    def create(self, request):
+        '''
+            Handles creating a new order when a user hits add to cart
+        '''
+        customer = Customer.objects.get(user=request.auth.user)
+        date = datetime.datetime.now()
+        newOrder = Order()
+        newOrder.customer = customer
+        newOrder.created_at = date
+        newOrder.payment_type_id = None
+
+        newOrder.save()
+
+        serialize = OrderSerializer(newOrder, context={'request': request})
+        return Response(serialize.data)
+    
+
+
 
 
     
