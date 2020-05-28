2400589e3c202745a13db57e91423873b34089d3
keithrpotempa <keithrpotempa@gmail.com>
2020-05-28 15:38:48 -0500

    added password to the user update view

--- ecommerceapi/views/customer/user.py
+++ ecommerceapi/views/customer/user.py
@@ -45,6 +45,7 @@ class Users(ViewSet):
         """
         user = User.objects.get(pk=pk)
         user.username = request.data["username"]
+        user.password = request.data["password"]
         user.first_name = request.data["first_name"]
         user.last_name = request.data["last_name"]
         user.email = request.data["email"]
