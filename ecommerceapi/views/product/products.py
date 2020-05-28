35ee40b61c1b1b6fe39550181ac19ff40e305100
Andrew Green <green.andrew.b@gmail.com>
2020-05-28 11:46:59 -0500

    delete product

--- ecommerceapi/views/product/products.py
+++ ecommerceapi/views/product/products.py
@@ -85,4 +85,16 @@ class Products(ViewSet):
         pass
 
     def destroy(self, request, pk=None):
-        pass
+        # pass
+        try:
+            product = Product.objects.get(pk=pk)
+            product.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Product.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
