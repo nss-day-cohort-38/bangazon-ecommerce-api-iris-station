e9d0252a248bed38815cdfa6dc4c0b9beae863f7
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-06-03 11:53:23 -0500

    added multiple orders route

--- ecommerceapi/tests/testorders.py
+++ ecommerceapi/tests/testorders.py
@@ -1,30 +1,39 @@
 from django.test import TestCase
 from django.urls import reverse
-from ecommerceapi.models import Order, Customer
+from ecommerceapi.models import Order, Customer, PaymentType
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from unittest import skip
+
 
 class TestOrders(TestCase):
 
     def setUp(self):
         self.username = "TestUser"
         self.password = "testword1"
-        self.user =  User.objects.create_user(
+        self.user = User.objects.create_user(
             username=self.username, password=self.password)
         self.token = Token.objects.create(user=self.user)
-        self.customer = Customer.objects.create(user_id=1, address="111 test road", phone_number="5555555555")
+        self.customer = Customer.objects.create(
+            user_id=1, address="111 test road", phone_number="5555555555")
+        self.paymenttype = PaymentType.objects.create(
+            merchant_name= "Merchant Name", 
+            account_number="123456789",
+            expiration_date="2028-02-18",
+            customer_id=1,
+            created_at="2020-05-27 14:22:22.995592"
+        )
 
     def testDeleteOrder(self):
         new_order = Order.objects.create(
-            customer_id = 1,
-            payment_type_id = None,
-            created_at = "2020-05-29T14:42:51.221420Z"
+            customer_id=1,
+            payment_type_id=None,
+            created_at="2020-05-29T14:42:51.221420Z"
         )
 
         response = self.client.delete(
             reverse('order-detail', kwargs={'pk': 1}), HTTP_AUTHORIZATION='Token ' + str(self.token))
-        
+
         self.assertEqual(response.status_code, 204)
 
         response = self.client.get(
@@ -32,7 +41,38 @@ class TestOrders(TestCase):
 
         self.assertEqual(len(response.data), 0)
 
+    def testMultipleOrders(self):
+        order_one = Order.objects.create(
+            customer_id=1,
+            payment_type_id=None,
+            created_at="2020-05-29T14:42:51.221420Z"
+        )
+        order_two = Order.objects.create(
+            customer_id=1,
+            payment_type_id=None,
+            created_at="2020-05-29T14:42:51.221420Z"
+        )
 
+        response = self.client.get('/orders', {'open_count': 'customer', "open": "True"})
+
+        self.assertEqual(response.data[0]["open_count"], 2)
+
+    def testOpenOrders(self):
+        order_one = Order.objects.create(
+            customer_id=1,
+            payment_type_id=1,
+            created_at="2020-05-29T14:42:51.221420Z"
+        )
+
+        order_two = Order.objects.create(
+            customer_id=1,
+            payment_type_id=None,
+            created_at="2020-05-29T14:42:51.221420Z"
+        )
+
+        response = self.client.get('/orders', {"open": "True"})
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["id"], 2)
 
 
 if __name__ == '__main__':
--- ecommerceapi/views/order/orders.py
+++ ecommerceapi/views/order/orders.py
@@ -10,6 +10,7 @@ from rest_framework import serializers
 from rest_framework import status
 from ecommerceapi.models import Order, Customer
 from .. import PaymentSerializer
+from django.db.models import Count, F, When, Case, IntegerField
 
 
 class CustomerSerializer(serializers.HyperlinkedModelSerializer):
@@ -17,11 +18,12 @@ class CustomerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Customer
         url = serializers.HyperlinkedIdentityField(
-            view_name = 'customer',
-            lookup_field = "id"
+            view_name='customer',
+            lookup_field="id"
         )
         fields = ('id', 'address')
         depth = 1
+
 
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
     '''
@@ -29,38 +31,68 @@ class OrderSerializer(serializers.HyperlinkedModelSerializer):
     '''
     payment_type = PaymentSerializer('payment_type')
     customer = CustomerSerializer('customer')
+    open_count = serializers.SerializerMethodField()
+
+    def get_open_count(self, obj):
+        return (obj.open_count if hasattr(obj, "open_count") else None)
+
     class Meta:
         model = Order
         url = serializers.HyperlinkedIdentityField(
-            view_name = 'orders',
-            lookup_field = "id"
+            view_name='orders',
+            lookup_field="id"
         )
 
-        fields = ('id', 'payment_type_id', 'created_at', 'payment_type', "customer")
-    
+        fields = ('id', 'payment_type_id', 'created_at',
+                  'payment_type', "customer", "open_count")
+
 
 class Orders(ViewSet):
-    
-    def list(self, request):
 
+    def list(self, request):
         '''
         This function returns all of the orders associated with the current user, (based of token). It is important to note that it is ordered
         by date so the first item returned is the most recent and if that payment type id is null than it is an open cart
         '''
+        open = self.request.query_params.get('open', None)
+        open_count = self.request.query_params.get('open_count', None)
+
         customer = None
-        if hasattr( request.auth, "user"):
+        if hasattr(request.auth, "user"):
             customer = Customer.objects.get(user=request.auth.user)
 
         orders = Order.objects.all()
         if customer is not None:
-            orders = orders.filter(customer = customer)
+            orders = orders.filter(customer=customer)
+
+        if open is not None:
+            orders = orders.filter(payment_type__isnull=True)
+
+        if open_count is not None:
+            newDict = {}
+            for order in orders:
+                customer_id = order.customer.id
+                if customer_id in newDict:
+                    newDict[customer_id] += 1
+                else:
+                    newDict[customer_id] = 1
+                    
+            whens = [
+                When(customer=k, then=v) for k, v in newDict.items()
+            ]
+
+            orders = orders.annotate(open_count=Case(
+                *whens, default=0, output_field=IntegerField())).filter(open_count__gt=1)
+
         paymentType = self.request.query_params.get('payment_type_id', None)
+
         if paymentType is not None:
-            orders.filter(payment_type_id = paymentType)
-        serializer = OrderSerializer(orders, many=True, context={'request': request})
+            orders.filter(payment_type_id=paymentType)
+        serializer = OrderSerializer(
+            orders, many=True, context={'request': request})
 
         return Response(serializer.data)
-    
+
     def create(self, request):
         '''
             Handles creating a new order when a user hits add to cart
@@ -76,7 +108,6 @@ class Orders(ViewSet):
 
         serialize = OrderSerializer(newOrder, context={'request': request})
         return Response(serialize.data)
-    
 
     def update(self, request, pk=None):
 
@@ -85,7 +116,7 @@ class Orders(ViewSet):
 
         ogOrder.save()
         return Response({}, status=status.HTTP_204_NO_CONTENT)
-    
+
     def destroy(self, request, pk=None):
 
         try:
@@ -93,11 +124,9 @@ class Orders(ViewSet):
             ogOrder.delete()
 
             return Response({}, status=status.HTTP_204_NO_CONTENT)
-        
+
         # except Order.DoesNotExist as ex:
         #     return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
 
         except Exception as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    
