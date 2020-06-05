1d399ab859bf54033dd393c007539c5c32cc6dde
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-06-05 15:17:56 -0500

    added register test

--- ecommerceapi/views/customer/register.py
+++ ecommerceapi/views/customer/register.py
@@ -6,6 +6,7 @@ from rest_framework.authtoken.models import Token
 from django.views.decorators.csrf import csrf_exempt
 from ecommerceapi.models import Customer
 
+
 @csrf_exempt
 def register_user(request):
     '''Handles the creation of a new user for authentication
@@ -19,26 +20,30 @@ def register_user(request):
 
     # Create a new user by invoking the `create_user` helper method
     # on Django's built-in User model
-    new_user = User.objects.create_user(
-        username=req_body['username'],
-        email=req_body['email'],
-        password=req_body['password'],
-        first_name=req_body['first_name'],
-        last_name=req_body['last_name']
-    )
+    try:
+        new_user = User.objects.create_user(
+            username=req_body['username'],
+            email=req_body['email'],
+            password=req_body['password'],
+            first_name=req_body['first_name'],
+            last_name=req_body['last_name']
+        )
 
-    customer = Customer.objects.create(
-        user=new_user,
-        address=req_body['address'],
-        phone_number=req_body['phone_number']
-    )
+        customer = Customer.objects.create(
+            user=new_user,
+            address=req_body['address'],
+            phone_number=req_body['phone_number']
+        )
 
-    # Commit the user to the database by saving it
-    customer.save()
+        # Commit the user to the database by saving it
+        customer.save()
 
-    # Use the REST Framework's token generator on the new user account
-    token = Token.objects.create(user=new_user)
+        # Use the REST Framework's token generator on the new user account
+        token = Token.objects.create(user=new_user)
 
-    # Return the token to the client
-    data = json.dumps({"token": token.key})
-    return HttpResponse(data, content_type='application/json')
+        # Return the token to the client
+        data = json.dumps({"token": token.key})
+        return HttpResponse(data, content_type='application/json')
+        
+    except Exception as x:
+        return HttpResponse(x, content_type='application/json')
