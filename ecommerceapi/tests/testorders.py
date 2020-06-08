e713d2e3e93a1679af8981a0aa4154144f986344
Kurt Krafft <kkrafft6609@gmail.com>
2020-06-08 13:30:11 -0500

    :whoops gi

--- ecommerceapi/tests/testorders.py
+++ ecommerceapi/tests/testorders.py
@@ -4,6 +4,7 @@ from ecommerceapi.models import Order, Customer, PaymentType
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from unittest import skip
+import json
 
 """
 - Np Retrieve
@@ -43,20 +44,66 @@ class TestOrders(TestCase):
             created_at="2020-05-29T14:42:51.221420Z"
         )
 
-        response = self.client.delete(
-            reverse('order-detail', kwargs={'pk': 1}), HTTP_AUTHORIZATION='Token ' + str(self.token))
+        response = self.client.get(
+            reverse('order-list'), HTTP_AUTHORIZATION='Token ' + str(self.token))
         
         # test that there is only one order that has been created 
         self.assertEqual(len(response.data), 1)
+        
+        self.assertEqual(response.status_code, 200)
 
         # test that it is indeed the one we created 
-        self.assertEqual(response.data[0]["created_at"], "2020-05-29T14:42:51.221420Z")
+        self.assertEqual(response.data[0]["customer"]["id"], 1)
     
     def testList(self):
-        pass
+        new_order = Order.objects.create(
+            customer_id=1,
+            payment_type_id=None,
+            created_at="2020-05-29T14:42:51.221420Z"
+        )
+        second_order = Order.objects.create(
+            customer_id=1,
+            payment_type_id=None,
+            created_at="2019-06-29T14:42:51.221420Z"
+        )
+
+
+        response = self.client.get(
+            reverse('order-list'), HTTP_AUTHORIZATION='Token ' + str(self.token))
+
+        # test for two orders returning under length
+
+        self.assertEqual(len(response.data), 2)
+
+        self.assertEqual(response.status_code, 200)
 
     def testEdit(self):
-        pass
+        new_order = Order.objects.create(
+            customer_id=1,
+            payment_type_id=None,
+            created_at="2020-05-29T14:42:51.221420Z"
+        )
+        pt = PaymentType.objects.create(
+            merchant_name="Stupid Company", 
+            account_number="1234123412341234", 
+            expiration_date="2024-01-01", 
+            customer_id=1, 
+            created_at="2020-05-27 15:08:30.518598Z")
+
+        updated_order ={
+            "customer_id":1,
+            "payment_type_id":1,
+            "created_at":"2020-05-29T14:42:51.221420Z"
+        }
+        response = self.client.put(
+            reverse('order-detail', kwargs={'pk': 1}),
+            updated_order,
+            content_type='application/json',
+            HTTP_AUTHORIZATION='Token ' + str(self.token)
+        )
+
+
+        self.assertEqual(response.status_code, 204)
     
     def testPaymentTypeIdQuery(self):
         pass
