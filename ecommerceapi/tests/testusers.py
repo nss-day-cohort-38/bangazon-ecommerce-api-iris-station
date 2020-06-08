21b1cca21d5e8d17c4d95ac6c503852c09623159
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-06-08 12:10:06 -0500

    added test get for user

--- ecommerceapi/tests/testusers.py
+++ ecommerceapi/tests/testusers.py
@@ -9,10 +9,34 @@ from unittest import skip
 class TestUser(TestCase):
     # Set up all data that will be needed to excute all the tests in the test file.
     def setUp(self):
-        pass
+        self.user = User.objects.create_user(
+            username="JohnnyBoy",
+            password="Test123",
+            email="JohnnyBoy@me.com",
+            first_name="Johnny",
+            last_name="Boy")
 
     def testGet(self):
-        pass
+        # Get request for user created in setup
+        response = self.client.get("/users/1")
+
+        # Checks to make sure get request went through
+        self.assertEqual(response.status_code, 200)
+        data = response.data
+
+        # Checks to make sure all data is correct
+        self.assertEqual(data["first_name"], "Johnny")
+        self.assertEqual(data["last_name"], "Boy")
+        self.assertEqual(data["username"], "JohnnyBoy")
+        self.assertEqual(data["email"], "JohnnyBoy@me.com")
+
+        # Get request for user that does not exist
+        response = self.client.get("/users/2")
+
+        # Checks that error is correct if user does not exist
+        # Content is in bit so .decode conerts bytes to string
+        self.assertEqual(response.content.decode("utf-8"),
+                         "User matching query does not exist.")
 
     def testEdit(self):
         pass
