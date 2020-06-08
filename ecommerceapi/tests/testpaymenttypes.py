af699f34bc8ae605b741e1c83772289849c1a446
Kurt Krafft <kkrafft6609@gmail.com>
2020-06-08 11:26:31 -0500

    added pytypes testing

--- ecommerceapi/tests/testpaymenttypes.py
+++ ecommerceapi/tests/testpaymenttypes.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from ecommerceapi.models import PaymentType
+from ecommerceapi.models import PaymentType, Customer
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from unittest import skip
@@ -8,19 +8,81 @@ from unittest import skip
 class TestPaymentTypes(TestCase):
     # Set up all data that will be needed to excute all the tests in the test file.
     def setUp(self):
-        pass
+        self.username = "TestUser"
+        self.password = "testword1"
+        self.user =  User.objects.create_user(
+            username=self.username, password=self.password)
+        self.token = Token.objects.create(user=self.user)
+        self.customer = Customer.objects.create(user_id=1, address="111 test road", phone_number="5555555555")
 
     def testPost(self):
-        pass
+        pt = PaymentType.objects.create(
+            merchant_name="Stupid Company", 
+            account_number="1234123412341234", 
+            expiration_date="2024-01-01", 
+            customer_id=1, 
+            created_at="2020-05-27 15:08:30.518598Z")
+
+        response = self.client.get(
+            reverse('paymenttypes-list'), HTTP_AUTHORIZATION='Token ' + str(self.token))
+
+        self.assertEqual(response.status_code, 200)
+        
+        self.assertEqual(len(response.data), 1)
+
+        self.assertEqual(response.data[0]["merchant_name"], "Stupid Company")
     
     def testList(self):
-        pass
+        pt = PaymentType.objects.create(
+            merchant_name="Stupid Company", 
+            account_number="1234123412341234", 
+            expiration_date="2024-01-01", 
+            customer_id=1, 
+            created_at="2020-05-27 15:08:30.518598Z")
+
+        response = self.client.get(
+            reverse('paymenttypes-list'), HTTP_AUTHORIZATION='Token ' + str(self.token))
+        
+        self.assertEqual(response.status_code, 200)
+        
+        self.assertEqual(len(response.data), 1)
+        
 
     def testGet(self):
-        pass
+        pt = PaymentType.objects.create(
+            merchant_name="Stupid Company", 
+            account_number="1234123412341234", 
+            expiration_date="2024-01-01", 
+            customer_id=1, 
+            created_at="2020-05-27 15:08:30.518598Z")
+
+        response = self.client.get(
+            reverse('paymenttypes-list'), HTTP_AUTHORIZATION='Token ' + str(self.token))
+        
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.data[0]["id"], 1)
+
+        self.assertIn(pt.merchant_name.encode(), response.content)
+
 
     def testDelete(self):
-        pass
+        pt = PaymentType.objects.create(
+            merchant_name="Stupid Company", 
+            account_number="1234123412341234", 
+            expiration_date="2024-01-01", 
+            customer_id=1, 
+            created_at="2020-05-27 15:08:30.518598Z")
+
+        response = self.client.delete(
+            reverse('paymenttypes-detail', kwargs={'pk': 1}), HTTP_AUTHORIZATION='Token ' + str(self.token))
+        
+        self.assertEqual(response.status_code, 204)
+
+        response = self.client.get(
+            reverse('paymenttypes-list'), HTTP_AUTHORIZATION='Token ' + str(self.token))
+
+        self.assertEqual(len(response.data), 0)
 
 
 
